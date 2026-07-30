6a96f2a276bee5413c57c2e7eb28afe46b1e366a
ThanhPhammmm <phamvanthanh2522004@gmail.com>
2026-07-30 09:17:28 +0700

    modify implementation

--- Drone/Core/Inc/SensorDriver/QMC5883/qmc5883.h
+++ Drone/Core/Inc/SensorDriver/QMC5883/qmc5883.h
@@ -4,7 +4,7 @@
 #include "qmc5883_data_types.h"
 
 typedef struct{
-    QMC5883_Raw_t   raw;
+    QMC5883_Raw_t	raw;
     QMC5883_Field_t field;
     QMC5883_Calib_t calib;
 } QMC5883_Handle_t;
@@ -12,7 +12,10 @@ typedef struct{
 extern QMC5883_Handle_t qmc5883;
 
 QMC5883_Status_t QMC5883_Init(void);
+uint8_t	QMC5883_DataReady(void);
 QMC5883_Status_t QMC5883_Read(void);
-QMC5883_Status_t QMC5883_Calibrate(uint32_t durationMs);
+void QMC5883_CalibReset(void);
+void QMC5883_CalibAccumulate(void);
+void QMC5883_CalibFinish(void);
 
 #endif /* INC_SENSORDRIVER_QMC5883_QMC5883_H_ */
